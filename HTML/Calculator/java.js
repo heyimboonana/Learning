06738b19f1cf46e61b77e81f2aa097abc38e6392
heyimboonana <alia762506@gmail.com>
2025-11-23 22:47:40 -0800

    java func

--- HTML/Calculator/java.js
+++ HTML/Calculator/java.js
@@ -3,5 +3,17 @@ function calculate(){
     const y = parseInt(document.getElementById("int2").value);
     let z = x + y;
     z = "Hello world"
+
+
     document.getElementById("demo").innerHTML = z;
+}
+function helloPrint(){
+    // add image only once
+    if (!document.getElementById("myImage")) {
+        const imageContainer = document.getElementById("image-display-area");
+        const newImage = document.createElement("img");
+        newImage.id = "myImage";
+        newImage.src = "https://i.programmerhumor.io/2025/08/972f2fdfbfdb40d50f356754ba8aa0f242f9ecf2ac1b8c9c1b6c064976373aeb.jpeg";
+        imageContainer.appendChild(newImage);
+    }
 }
