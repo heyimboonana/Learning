473cc013fd19a7ca50ad7144637a2ffb79d6a0b8
heyimboonana <alia762506@gmail.com>
2025-11-23 22:22:48 -0800

    calculate

---
+++ HTML/Calculator/java.js
@@ -0,0 +1,7 @@
+function calculate(){
+    const x = parseInt(document.getElementById("int1").value);
+    const y = parseInt(document.getElementById("int2").value);
+    let z = x + y;
+    z = "Hello world"
+    document.getElementById("demo").innerHTML = z;
+}
