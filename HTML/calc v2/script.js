db106d98c9fe9046f1884e0c40a431e627a98e7a
heyimboonana <alia762506@gmail.com>
2025-11-24 20:46:18 -0800

    script

--- HTML/calc v2/script.js
+++ HTML/calc v2/script.js
@@ -2,7 +2,9 @@ let currentOperation = '';
 let x = '';
 let y = '';
 let result = '';
+let prevResult = '';
 
+//Add Numbers to display and to variables depending if operater has been pressed
 function appendNumber(num){
     if (result == ''){
         if (currentOperation == ''){
@@ -15,14 +17,56 @@ function appendNumber(num){
         }
     } 
 }
+
+//opperater function
 function appendOperation(operator){
-    if (document.getElementById('display').value == ''){
+    //if C is pressed, Clear display and make all variables blank
+    if (operator == 'C'){
+        document.getElementById('display').value = ''
+        currentOperation = '';
+        x = '';
+        y = '';
+        result = '';
+    }
+
+    //if DEL is pressed, remove last character from the display and update variable
+    if (operator === 'DEL'){
+        const disp = document.getElementById('display').value;
+        document.getElementById('display').value = disp.slice(0,-1);
+        if(y !== ''){
+            y = y.slice(0,-1);
+        }
+        else if(currentOperation !== ''){
+            currentOperation = ''
+        }
+        else if(x !== ''){
+            x = x.slice(0,-1);
+        }
+    }
+
+    //if NEGATIVE is pressed, it will make it negative if positive and vice versa
+    if (operator == 'NEGATIVE'){
+        if(y !== ''){
+            y = -y;
+        }
+        else if(x !== ''){
+            x = -x;
+        }
+        document.getElementById('display').value = x + currentOperation + y;
+    }
+
+    //if display is blank and operator is pressed, return and not output anything
+    else if (document.getElementById('display').value == ''){
         return;
     }
-    else if (currentOperation == ''){
+
+    //update operator if numbers have been pressed 
+    else if (currentOperation == '' && currentOperation !== 'DEL' && currentOperation !== '='){
         document.getElementById('display').value += operator
         currentOperation = operator;
     }
+
+
     else if(operator == "="){
         if (x == '' || y == '' ){
             return;
@@ -35,6 +79,18 @@ function appendOperation(operator){
 function calculate(){
     if (currentOperation == '+'){
         result = parseInt(x) + parseInt(y);
+        document.getElementById('display').value = result;
+    }
+    if (currentOperation == '-'){
+        result = parseInt(x) - parseInt(y);
+        document.getElementById('display').value = result;
+    }
+    if (currentOperation == '*'){
+        result = parseInt(x) * parseInt(y);
+        document.getElementById('display').value = result;
+    }
+    if (currentOperation == '/'){
+        result = parseInt(x) / parseInt(y);
         document.getElementById('display').value = result;
     }
 }
