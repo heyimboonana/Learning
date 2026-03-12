f56feb65c0c6e3a0d0d7718af6cd750c6bf70e17
Osvaldo Hernandez <76hernandezo@gmail.com>
2026-03-12 08:23:40 -0700

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main() {
     int x;
-    cout << "Pick a number:";
+    int y;
+
+    cout << "Pick the first number:";
     cin >> x;
+
+    cout << "Pick the secong number:";
+    cin >> y;
+
+    cout <<
     return 0;
 } 
