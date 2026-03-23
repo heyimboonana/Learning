be7e7960cf2b6e626ef172743961786d8db9e685
Osvaldo Hernandez <76hernandezo@gmail.com>
2026-03-22 21:21:22 -0700

    c

--- c++/Num Guessing game/main.cpp
+++ c++/Num Guessing game/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
 
 int main(){
-    
+    int random = rand() % 100 + 1;
+    int guess;
+
+    //cout << "Random: " << random ;
+    do{
+        cout << "guess a number: ";
+        cin >> guess;
+    }while((guess < 0));
+
+    return 0;
 }
