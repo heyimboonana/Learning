acbcee4711fba9d0bbb809960c714b4566af79e3
Osvaldo Hernandez <76hernandezo@gmail.com>
2026-03-23 21:11:06 -0700

    Higher or lower

--- c++/Num Guessing game/main.cpp
+++ c++/Num Guessing game/main.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <cstdlib>
-
-using namespace std;
+#include <limits>
 
 int main(){
+    srand(time(0));
     int random = rand() % 100 + 1;
     int guess;
+    int tries = 1;
 
-    //cout << "Random: " << random ;
-    do{
-        cout << "guess a number: ";
-        cin >> guess;
-    }while((guess < 0));
+    //std::cout << "Random: " << random << "\n";
+    std::cout << "Enter a guess: ";
+    
 
+    while(guess != random){
+        while (!(std::cin >> guess)) {
+            std::cout << "Invalid input. Please try again: ";
+                
+            // 1. Clear the error flags
+            std::cin.clear(); 
+                
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        if(guess == random){
+            std::cout << "CORRECT!" << "\n";
+        }
+        else if(guess > random){
+            tries++;
+            std::cout << "Lower" << "\n";
+            std::cout << "Guess:";
+        }
+        else if(guess < random){
+            tries++;
+            std::cout << "Higher" << "\n";
+            std::cout << "Guess:";
+        }
+    }
+    std::cout<< "Tries: " << tries;
     return 0;
 }
