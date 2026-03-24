ac458c685e07dbd7f850d9d4112cef3057a1c342
Osvaldo Hernandez <76hernandezo@gmail.com>
2026-03-23 23:47:05 -0700

    commiut

---
+++ c++/ConsoleCalculator/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    std:: string operation;
+    while(true){
+        while(!(operation == "+" || operation == "-" || operation == "*" || operation == "/")){
+            std::cout << "Enter an operation (+, -, *, / ): ";
+            std::cin >> operation;
+        }
+        while()
+    }
+    return 0;
+}
