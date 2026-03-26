ca1d4dc99936de869ed2f904468133de01314c7e
Osvaldo Hernandez <76hernandezo@gmail.com>
2026-03-26 16:10:49 -0700

    commit

--- c++/ConsoleCalculator/main.cpp
+++ c++/ConsoleCalculator/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <limits>
-#include <string>
+#include <cctype>
 
-//EQUATION FUNCTION
+//EQUATION
 int equation(int num1, int num2, char operation) {
     switch(operation) {
         case '+': return num1 + num2;
@@ -13,49 +13,58 @@ int equation(int num1, int num2, char operation) {
     }
 }
 
-//MAIN
-int main(){
-    int num1;
-    int num2;
-    int sum;
-    bool calcStart = true;
-
-    //INPUT NUMS
-    std:: cout << "(PRESS 'S' TO STOP)Enter the first number: ";
-
-    while(!(std::cin >> num1)){
+//GET NUM
+int getNum (){
+    int num;
+    while(!(std::cin >> num)){
         std:: cout << "(PRESS 'S' TO STOP)Invalid, enter a number: ";
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
-    if(num1 == -1){
-        return 0;
+    if(num == -1){
+        return -1;
     }
+    else{
+        return num;
+    }
+}
+
+//GET OPERATION
+char getOp(){
+    char op = '.';
+    //CHECK OPERATION
+    while(!(op == '+' || op == '-' || op == '*' || op == '/')){
+        std::cout << "(ENTER 'Q' TO END)Enter an operation (+, -, *, / ): ";
+        std::cin >> op;
+        if(static_cast<char>(std::toupper(static_cast<unsigned char>(op))) == 'Q'){
+            return 'Q';
+        }
+    }
+    return op;
+}
+
+//MAIN
+int main(){
+    int calculations = 1;
+    //INPUT NUMS
+    std:: cout << "(ENTER '-1' TO STOP)Enter the first number: ";
+    int num1 = getNum();
+    if(num1 == -1) return 0;
+
     std::cout << num1 << " ( ) _ " << "\n";
     while(true){
-        char operation = '.';
+        char op = getOp();
+        if(op == 'Q') break;
 
-        //CHECK OPERATION
-        while(!(operation == '+' || operation == '-' || operation == '*' || operation == '/')){
-            std::cout << "(ENTER '-1' TO END)Enter an operation (+, -, *, / ): ";
-            std::cin >> operation;
-            if(operation == '-1'){
-                return 0;
-            }
-        }
-        std::cout << num1 << " " << operation <<" _ " << "\n";
+        std::cout << num1 << " " << op <<" _ " << "\n";
         std:: cout << "(ENTER '-1' TO END)Enter the second number: ";
-        while(!(std::cin >> num2)){
-            std:: cout << "(ENTER '-1' TO END)Invalid, enter a number: ";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
-        if(num2 == -1){
-            return 0;
-        }
-        sum = equation(num1, num2, operation);
-        std:: cout << num1 << " " << operation << " " << num2 << " = " << sum << "\n";
-        num1 = sum;
+        int num2 = getNum();
+        if(num2 == -1) break;
+
+        int result = equation(num1, num2, op);
+        std:: cout << calculations << ". " <<  num1 << " " << op << " " << num2 << " = " << result << "\n";
+        calculations++;
+        num1 = result;
     }
     return 0;
 }
